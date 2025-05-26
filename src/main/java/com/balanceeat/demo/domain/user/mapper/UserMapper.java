bec5ea5c52eedc33ab806aec4fb179c3d04c17ba
이지민 <1zzimy@naver.com>
2025-05-26 14:27:15 +0900

    feat: users 테이블에 프로필 및 챌린지 참여 여부 추가
    - users 테이블에 profile_image_url, goal_message, is_challenge_enabled 필드 추가
    - User Entity, UserProfileDTO, UserMapper에 관련 필드 반영
    - getUserProfile 쿼리 및 매퍼 메서드 추가

--- src/main/java/com/balanceeat/demo/domain/user/mapper/UserMapper.java
+++ src/main/java/com/balanceeat/demo/domain/user/mapper/UserMapper.java
@@ -3,16 +3,17 @@ package com.balanceeat.demo.domain.user.mapper;
 import java.util.Optional;
 
 import com.balanceeat.demo.domain.user.dto.UserDTO;
+import com.balanceeat.demo.domain.user.dto.UserProfileDTO;
 import com.balanceeat.demo.domain.user.entity.User;
 import org.apache.ibatis.annotations.Mapper;
 
 @Mapper
 public interface UserMapper {
-    User getUserById(String id);
+    User getUserById(Long id);
+    UserProfileDTO getUserProfile(Long id);
     void updateUser(UserDTO userDTO);
-    void deleteUser(String id);
+    void deleteUser(Long id);
     Optional<User> findByEmail(String email);
     void insert(User user);
-    void updateUserIsActive(String id, boolean isActive);
     boolean existsByEmail(String email);
 }
